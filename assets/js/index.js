69f809cb664089b95fd5309ebbdb176c153cbe9a
Velisarios1234 <fafoutis@csd.auth.gr>
2022-03-30 11:17:18 +0300

    project updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,34 @@
-  // initialize Leaflet
-  var map = L.map('map').setView({lon: 0, lat: 0}, 2);
+// initialize Leaflet
+var map = L.map('map').setView({ lon: 0, lat: 0 }, 2);
+var counter = 0;
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
 
-  // add the OpenStreetMap tiles
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-  }).addTo(map);
-
-  // show the scale bar on the lower left corner
-  L.control.scale({imperial: true, metric: true}).addTo(map);
-
-  // show a marker on the map
-  L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+// show the scale bar on the lower left corner
+L.control.scale({ imperial: true, metric: true }).addTo(map);
+counter++;
+// show a marker on the map
+//L.marker({ lon: 0, lat: 0 }).bindPopup('The center of the world').addTo(map);
 
 
 
-const geoData= [];
+const geoDataAdded = new Array(39);
 
-function loadGeoData(){
+function loadGeoData() {
 
-    fetch('/assets/json/europe.geo.json').then(response => {
-        return response.json();
-      }).then(data => {
-        // Work with JSON data here
-        generate_countries_list(data);
-       
-      }).catch(err => {
-        // Do something for an error here
-      });
-    
+  fetch('/assets/json/europe.geo.json').then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    generate_countries_list(data);
+
+  }).catch(err => {
+    // Do something for an error here
+  });
+
 }
 
 
@@ -37,68 +37,143 @@ function loadGeoData(){
 
 
 
-function generate_countries_list(json_data){
-    var countries_list = document.getElementById("countries-list");
+function generate_countries_list(json_data) {
+  var countries_list = document.getElementById("countries-list");
 
-    const features = json_data['features'];
-    for(let  i = 0; i < features.length; i++){
-        var list_element = document.createElement('li');
-        list_element.setAttribute("class", "list-element");
-        const feuture_properties = features[i]['properties'];
-       
-        const wb_a2 = feuture_properties['wb_a2'];
-        list_element.setAttribute("id", wb_a2);
-        list_element.setAttribute("onclick", "addCountryPolygonToMap(this.id)");
-        const formal_en = feuture_properties['formal_en'];
-        list_element.innerHTML = formal_en;
+  const features = json_data['features'];
+  for (let i = 0; i < features.length; i++) {
+    var list_element = document.createElement('li');
+    list_element.setAttribute("class", "list-element");
+    const feuture_properties = features[i]['properties'];
 
-       
+    const wb_a2 = feuture_properties['wb_a2'];
+    list_element.setAttribute("id", wb_a2);
+    list_element.setAttribute("onclick", "addCountryPolygonToMap(this.id)");
+    const formal_en = feuture_properties['formal_en'];
+    list_element.innerHTML = formal_en;
 
-        countries_list.appendChild(list_element);
+
+
+    countries_list.appendChild(list_element);
+  }
 }
+
+
+function addCountryToMap(wb_a2_to_search) {
+ 
+  var feature_properties = [];
+  fetch('/assets/json/europe.geo.json').then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    var features = data['features'];
+    for (let i = 0; i < features.length; i++) { 
+      var properties = features[i]['properties'];
+      var wb_a2 = properties['wb_a2'];
+     
+      if (wb_a2 == wb_a2_to_search) {
+        geoDataAdded[i] = true;
+        L.geoJSON(features[i]).addTo(map);
+        
+       break;
+      
+      }
+    }
+
+  }).catch(err => {
+    // Do something for an error here
+  });
+
+ 
+
 }
 
+function addCountryPolygonToMap(id) {
+  if(geoDataAdded.includes(id)){
+  
+    showPopUp();
+  // removeCountry(id);
+  }else{
+  
+    geoDataAdded.push(id);
+    addCountryToMap(id);
+  }
+  
 
-function getCountryProperties(wb_a2_to_search){
 
-    const feature_properties = [];
-    fetch('/assets/json/europe.geo.json').then(response => {
-        return response.json();
-      }).then(data => {
-        // Work with JSON data here
-        const features = data['features'];
-        for(let  i = 0; i < features.length; i++){
-            const properties = features[i]['properties'];
-            const wb_a2 = properties['wb_a2'];
-         
-            if(wb_a2 == wb_a2_to_search){
-                console.log('found ' + wb_a2 + '==' + wb_a2_to_search)
-                feature_properties = properties;
-            
-            }
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function showPopUp(){
+ 
+  document.getElementById('title').innerHTML = 'Country Already Added!';
+  delay(2000).then(() => document.getElementById('title').innerHTML = 'Welcome to my interactive map!');
+   
+}
+/*
+function removeCountry(wb_a2_to_search){
+  var feature_properties = [];
+  fetch('/assets/json/europe.geo.json').then(response => {
+    return response.json();
+  }).then(data => {
+    // Work with JSON data here
+    var features = data['features'];
+    for (let i = 0; i < features.length; i++) { 
+      var properties = features[i]['properties'];
+      var wb_a2 = properties['wb_a2'];
+     
+      if (wb_a2 == wb_a2_to_search) {
+        const index = geoDataAdded.indexOf(wb_a2_to_search);
+        if (index > -1) {
+          geoDataAdded.splice(index, 1); // 2nd parameter means remove one item only
         }
        
-      }).catch(err => {
-        // Do something for an error here
-      });
      
-      console.log(feature_properties);
-    return feature_properties;
-
-}
-
-function addCountryPolygonToMap(id){
-    const countryProperties = getCountryProperties(id);
-  
-    if(countryProperties != -99){
        
-        L.geoJSON(countryProperties).addTo(map);
-    }
+      // L.geoJSON(features[i]).clearLayer();
+      //  feature_properties = ;
+
     
+    }  
+       break;
+      
+      }
+    }
+
+  ).catch(err => {
+    // Do something for an error here
+  });
+}*/
+
+
+
+function clearMap(){
+  map.eachLayer(function (layer) {
+    map.removeLayer(layer);
+});
+
+
+
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+
+// show the scale bar on the lower left corner
+if(counter == 0){
+  L.control.scale({ imperial: true, metric: true }).addTo(map);
+  counter++;
 }
 
 
-
+while(geoDataAdded.length > 0) {
+  geoDataAdded.pop();
+}
+}
 
 loadGeoData();
 
